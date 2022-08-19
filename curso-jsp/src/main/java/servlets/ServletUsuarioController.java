45ab8197f8a2abfcb54325e4aa429fde9e9194af
Fabricio-Oliveira <fabricio001400@gmail.com>
2022-08-18 21:17:30 -0300

    Gerando gráfico e adicionando opção de filtro por período de data

--- curso-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ curso-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -339,7 +339,6 @@ public class ServletUsuarioController extends ServletGenericUtil {
 			request.setAttribute("msg", e.getMessage());
 			redirecionar.forward(request, response);
 		}
-		System.out.println("teste");
 	}
 
 }
