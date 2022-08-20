20d0dc9311567368c2bc424be2b6dd8f76e5ef64
Fabricio-Oliveira <fabricio001400@gmail.com>
2022-08-20 15:36:48 -0300

    Deploy do projeto para o Heroku (Tomcat 10)

--- curso-jsp/src/main/java/connection/SingleConnectionBanco.java
+++ curso-jsp/src/main/java/connection/SingleConnectionBanco.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class SingleConnectionBanco {/*Se fecha sess�es e transa��es*/
 	
-	private static String banco = "jdbc:postgresql://localhost:5432/cursojsp?autoReconnect=true";
-	private static String user = "postgres";
-	private static String senha = "admin";
+	private static String banco = "jdbc:postgresql://ec2-44-205-64-253.compute-1.amazonaws.com:5432/dbo9mmuvkkiipc?sslmode=require&autoReconnect=true";
+	private static String user = "tvvyzablxhnkhy";
+	private static String senha = "37fad8e698e7a7a855d80b34a8c4cddd856effa9d9799fbfaa36d973ee97d9ec";
 	private static Connection connection = null;
 	
 	
